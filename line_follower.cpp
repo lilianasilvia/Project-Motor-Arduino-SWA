50ec6a2cf70f1fc00d5b56fa443fdb80c08988ef
Liliana Vas <lilianasilvia.vas@gmail.com>
2020-05-10 11:46:48 +0300

    optimize code for car movement

--- line_follower.cpp
+++ line_follower.cpp
@@ -2,8 +2,8 @@
 
 int s1 = 3;
 int s2 = 4;
-int s3 = 5;
-int s4 = 6;
+int s3 = 2;
+int s4 = 9;
 int s5 = 7;
 int near = 8;
 
@@ -19,14 +19,14 @@ void IR_senzor_setup() {
 }
 
 void line_follow() {
-  int senzor1 = digitalRead(s1);  //with obstacle - LED OFF - 1 pin value
-  int senzor2 = digitalRead(s2);  // No obstacle - LED ON  - 0 pin value
+  int senzor1 = digitalRead(s1);  //with obstacle (does not sense the line) - LED OFF - 1 pin value
+  int senzor2 = digitalRead(s2);  // No obstacle(senses the line) - LED ON  - 0 pin value
   int senzor3 = digitalRead(s3);
   int senzor4 = digitalRead(s4);
   int senzor5 = digitalRead(s5);
   int senzorNear = digitalRead(near);
 
-  if (senzorNear == 0)
+   if (senzorNear == 0)
   {
     if (senzor3 == 0 && senzor1 == 1 && senzor2 == 1 && senzor4 == 1 && senzor5 == 1)
     {
@@ -35,32 +35,42 @@ void line_follow() {
 
     if (senzor2 == 0 && senzor1 == 1)
     {
-      controlDirection(5); //wide left
+      controlDirection(6); //wide left5
     }
 
     if (senzor1 == 0 && senzor2 == 1)
     {
-      controlDirection(6); //tight left
+      controlDirection(6); //tight left6
     }
 
     if (senzor1 == 0 && senzor2 == 0)
     {
-      controlDirection(6); //right left
+      controlDirection(6); //tight left6
+    }
+
+    if (senzor1 == 0 && senzor2 == 0 && senzor4 == 0)
+    {
+      controlDirection(6); //tight left6
     }
 
     if (senzor4 == 0 && senzor5 == 1)
     {
-      controlDirection(3); //wide right
+      controlDirection(4); //wide right3
     }
 
     if (senzor5 == 0 && senzor4 == 1)
     {
-      controlDirection(4); //tight right
+      controlDirection(4); //tight right4
     }
 
     if (senzor4 == 0 && senzor5 == 0)
     {
-      controlDirection(4); //tight right
+      controlDirection(4); //tight right4
+    }
+
+    if (senzor4 == 0 && senzor5 == 0 && senzor2 == 0)
+    {
+      controlDirection(4); //tight right4
     }
 
   }
@@ -71,5 +81,4 @@ void line_follow() {
 
 
 }
-
 
